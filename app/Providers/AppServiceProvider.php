8ea203df6fd77bd791e34db23ea7963849d940d9
jjr23reig <jjr23042000@gmail.com>
2021-06-11 13:02:51 +0200

    make it better

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot (UrlGenerator $url)
     {
-        if (env("REDIRECT_HTTPS") $url->formatScheme("https") ;
-    }   
-}
+        if (env("REDIRECT_HTTPS") $url->formatScheme("https://") ;
+    }
