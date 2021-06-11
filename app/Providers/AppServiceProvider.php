b701520fbeb65cfeb79d8ca4aadfe9fdced8c6f9
jjr23reig <jjr23042000@gmail.com>
2021-06-11 12:35:31 +0200

    make it better

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Laravel\Cashier\Cashier;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         Cashier::ignoreMigrations();
+        if (env("REDIRECT_HTTPS") $this->app["request"]->server->set("HTTPS", true) ;
     }
 
     /**
@@ -22,8 +24,8 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot (UrlGenerator $url)
     {
-        //
-    }
+        if (env("REDIRECT_HTTPS") $url->formatScheme("https") ;
+    }   
 }
